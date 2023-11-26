56c7d6de59dc3f59a2c18858bc1eccc55990d441
Gabriela Menezes <gabimenezess10@gmail.com>
2023-11-26 16:33:16 -0300

    style: tags

--- src/components/Gallery/Tags/index.jsx
+++ src/components/Gallery/Tags/index.jsx
@@ -1,7 +1,31 @@
+import styled from 'styled-components';
+import tags from './tags.json'
+const TagTitulo = styled.h3`
+    color: #D9D9D9;
+    font-size: 24px;
+    margin: 0;
+`;
 
+const Tag = styled.button`
+    font-size: 24px;
+    color: #FFFFFF;
+    background: rgba(217, 217, 217, 0.3);
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 12px;
+    box-sizing: border-box;
+    border: 2px solid transparent;
+    &:hover {
+      border-color: #C98CF1;
+    }
+`
 const Tags = () =>  {
   return (
-    <p>Tags</p>
+    <>
+        <p>Busque por tags:</p>
+        {tags.map(tag => <Tag key={tag.id}><TagTitulo>{tag.titulo}</TagTitulo></Tag>)}
+    </>
   )
 }
 
