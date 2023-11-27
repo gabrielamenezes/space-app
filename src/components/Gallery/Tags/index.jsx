1d351dbc90be199dbbed2d94258aee1eca12ebff
Gabriela Menezes <gabimenezess10@gmail.com>
2023-11-27 13:45:14 -0300

    style: finish styling tags

--- src/components/Gallery/Tags/index.jsx
+++ src/components/Gallery/Tags/index.jsx
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
 import tags from './tags.json'
+import './style.css'
 const TagTitulo = styled.h3`
     color: #D9D9D9;
     font-size: 24px;
     margin: 0;
 `;
+const ContainerSectionTags = styled.section`
+  display: flex;
+  margin: 66px 0 49px;
 
+`
+const ContainerTags = styled.div`
+  display: flex;
+  gap: 24px;
+  align-items: center;
+`
 const Tag = styled.button`
     font-size: 24px;
     color: #FFFFFF;
@@ -22,10 +32,12 @@ const Tag = styled.button`
 `
 const Tags = () =>  {
   return (
-    <>
+    <ContainerSectionTags>
         <p>Busque por tags:</p>
-        {tags.map(tag => <Tag key={tag.id}><TagTitulo>{tag.titulo}</TagTitulo></Tag>)}
-    </>
+        <ContainerTags>
+          {tags.map(tag => <Tag key={tag.id}><TagTitulo>{tag.titulo}</TagTitulo></Tag>)}
+        </ContainerTags>
+    </ContainerSectionTags>
   )
 }
 
